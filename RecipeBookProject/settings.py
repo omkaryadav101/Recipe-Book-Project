b5f3adb27a35da4ab41df677f5f96ade6b02fffa
Omkar Yadav <omkaryad.101@gmail.com>
2024-11-15 20:27:24 +0530

    Fix missing recipe_list.html template

--- RecipeBookProject/settings.py
+++ RecipeBookProject/settings.py
@@ -69,21 +69,26 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "RecipeBookProject.urls"
 
+
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR / 'templates',  # or specify the path to your app's templates
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = "RecipeBookProject.wsgi.application"
 
